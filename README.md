7eb04ef3c559ddf9c52363d3c8112b3e70aab8b8
Christian Salazar <christiansalazarh@gmail.com>
2013-12-21 20:23:09 -0430

    basic sql and readme

---
+++ README.md
@@ -0,0 +1,16 @@
+Object Modeling Framework (OMF)
+==============================
+
+As it name sugest it, is a framework for handling objects and the relationships between them. You can build
+an entire system using purely this framework.  
+
+
+**author:**
+
+Christian Salazar H. <christiansalazarh@gmail.com>
+
+**licence:**
+
+[http://opensource.org/licenses/bsd-license.php](http://opensource.org/licenses/bsd-license.php "http://opensource.org/licenses/bsd-license.php")
+
+
---
+++ omf-mysql.sql
@@ -0,0 +1,39 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE  TABLE IF NOT EXISTS `omf_object` (
+	  `id` INT NOT NULL AUTO_INCREMENT
+	, `classname` CHAR(20) NOT NULL
+	, `aux_id` VARCHAR(45) NULL
+	, `data` BLOB NULL
+	, PRIMARY KEY (`id`)
+	, INDEX `omf_object_classname_idx` (`classname` ASC)
+)ENGINE = InnoDB;
+
+CREATE  TABLE IF NOT EXISTS `omf_relationship` (
+	  `id` INT NOT NULL AUTO_INCREMENT
+	, `parent` INT NOT NULL
+	, `child` INT NOT NULL
+ 	, `name` VARCHAR(45) NOT NULL
+  	, `data` BLOB NULL
+	, PRIMARY KEY (`id`)
+	, INDEX `omf_relationship_parent_idx` (`parent` ASC)
+	, INDEX `omf_relationship_child_idx` (`child` ASC)
+	, INDEX `omf_relationship_parent_name_idx` (`parent` ASC , `name` ASC)
+	, INDEX `omf_relationship_child_name_idx` (`child` ASC , `name` ASC)
+  	, CONSTRAINT `fk_omf_object_relationship_parent`
+		FOREIGN KEY (`parent` )
+		REFERENCES `omf_object` (`id` )
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+  	, CONSTRAINT `fk_omf_object_relationship_child`
+		FOREIGN KEY (`child` )
+		REFERENCES `omf_object` (`id` )
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
